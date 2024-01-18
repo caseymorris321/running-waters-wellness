8d1e718bb895306f6d7b2e8814ce7a7320b22066
Casey Morris <morrisacasey@gmail.com>
2024-01-18 12:43:01 -0500

    public

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
 module.exports = {
     watch: ["src/**/*.html", "src/**/*.css", "src/**/*.js"],
-    isProduction: process.env.ELEVENTY_ENV === "PROD",
+    isProduction: process.env.ELEVENTY_ENV === "PROD"
 };
